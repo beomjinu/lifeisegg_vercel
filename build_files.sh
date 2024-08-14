af32498e66978d92a67e40c815e29ee12aa18d40
beomjinu <me@beomjinu.com>
2024-08-14 17:22:52 +0900

    edit wsgi.py

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,6 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install --no-cache-dir -r requirements.txt
 
-python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
 python3 manage.py collectstatic --noinput
--- config/wsgi.py
+++ config/wsgi.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'staticfiles'))
+
 app = application # for vercel
